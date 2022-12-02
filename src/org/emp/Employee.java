9f5233a4cf24d1b97d85449877fec00d8978dad9
Raja1220 <maarisathish007@gmail.com>
2022-12-02 19:41:03 +0530

    message

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,20 @@
+package org.emp;
+
+import java.util.Scanner;
+
+public class Employee {
+
+	 public static void main(String[] args) {
+		    Scanner s = new Scanner(System.in);
+		    int sum;
+		   int n1=s.nextInt();
+		   int n2=s.nextInt();
+		    do{
+		        sum=n1+n2;
+		    }while((n1>=0)||(n2>=0));
+		    System.out.println("Sum"+sum);
+		  }
+		}	
+	
+
+
